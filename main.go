cc64c404ed4e2c7c40fedae6e81ea164e5e4d97b
lozlof <lozlofcyber@gmail.com>
2024-10-26 22:37:35 +0000

    Updated main.go, created welcomeMessage.txt

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+// Use: go get -u github.com/Lozlof/lozgo to update to the newest lozgo repo.
+import (
+
+)
+
+func main() {
+	// Initialize userInput as a string.
+	var userInput string
+}
+
+// This function prints to the terminal.
+// inputChoice is used to determine what to print.
+func displayToTerm(inputChoice int) {
+	if inputChoice == 1 {
+		
+	}
+}
